1f9f9bb395d4d57bfc75cd7dc1f0bcf911a7fbd6
Abhimanyu Kushwaha <88226869+abhimanyu-kushwaha@users.noreply.github.com>
2021-09-07 15:04:14 +0530

    footer update

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="py-20 2xl:py-40 bg-gray-800">
+  <section class="py-10 2xl:py-10 bg-gray-800">
         <div class="container px-4 mx-auto">
           <div class="flex flex-wrap -mx-4 pb-24 mb-16 border-b border-gray-400">
             <div class="w-full lg:w-2/5 px-4 mb-16 lg:mb-0">
@@ -11,32 +11,38 @@
             <div class="w-full lg:w-3/5 px-4">
               <div class="flex flex-wrap -mx-4">
                 <div class="w-full md:w-1/2 lg:w-1/3 px-4 mb-16 lg:mb-0">
+                <h2 class="mb-3 text-gray-200 hover:text-gray-100">FOOD ORDERING</h2>
+                <hr class="mb-3"/>
                   <ul class="text-lg">
-                    <li class="mb-6"><a class="text-gray-200 hover:text-gray-100" href="#">Hello</a></li>
-                    <li class="mb-6"><a class="text-gray-200 hover:text-gray-100" href="#">Story</a></li>
-                    <li class="mb-6"><a class="text-gray-200 hover:text-gray-100" href="#">Pricing</a></li>
-                    <li class="mb-6"><a class="text-gray-200 hover:text-gray-100" href="#">Applictaions</a></li>
-                    <li class="mb-6"><a class="text-gray-200 hover:text-gray-100" href="#">Stats</a></li>
+                    <li class="mb-2"><a class="text-gray-200 hover:text-gray-100" href="#">Hello</a></li>
+                    <li class="mb-2"><a class="text-gray-200 hover:text-gray-100" href="#">Story</a></li>
+                    <li class="mb-2"><a class="text-gray-200 hover:text-gray-100" href="#">Pricing</a></li>
+                    <li class="mb-2"><a class="text-gray-200 hover:text-gray-100" href="#">Applictaions</a></li>
+                    <li class="mb-2"><a class="text-gray-200 hover:text-gray-100" href="#">Stats</a></li>
                     <li><a class="text-gray-200 hover:text-gray-100" href="#">Blog</a></li>
                   </ul>
                 </div>
                 <div class="w-full md:w-1/2 lg:w-1/3 px-4 mb-16 lg:mb-0">
+                 <h2 class="mb-3 text-gray-200 hover:text-gray-100">RESTAURANT SERVICES</h2>
+                <hr class="mb-3"/>
                   <ul class="text-lg">
-                    <li class="mb-6"><a class="text-gray-200 hover:text-gray-100" href="#">Newsletter</a></li>
-                    <li class="mb-6"><a class="text-gray-200 hover:text-gray-100" href="#">Features</a></li>
-                    <li class="mb-6"><a class="text-gray-200 hover:text-gray-100" href="#">How it works</a></li>
-                    <li class="mb-6"><a class="text-gray-200 hover:text-gray-100" href="#">FAQ</a></li>
-                    <li class="mb-6"><a class="text-gray-200 hover:text-gray-100" href="#">Portfolio</a></li>
+                    <li class="mb-2"><a class="text-gray-200 hover:text-gray-100" href="#">Newsletter</a></li>
+                    <li class="mb-2"><a class="text-gray-200 hover:text-gray-100" href="#">Features</a></li>
+                    <li class="mb-2"><a class="text-gray-200 hover:text-gray-100" href="#">How it works</a></li>
+                    <li class="mb-2"><a class="text-gray-200 hover:text-gray-100" href="#">FAQ</a></li>
+                    <li class="mb-2"><a class="text-gray-200 hover:text-gray-100" href="#">Portfolio</a></li>
                     <li><a class="text-gray-200 hover:text-gray-100" href="#">Team</a></li>
                   </ul>
                 </div>
                 <div class="w-full lg:w-1/3 px-4">
+                 <h2 class="mb-3 text-gray-200 hover:text-gray-100">PARTNER & SUPPORT</h2>
+                <hr class="mb-3"/>
                   <ul class="text-lg">
-                    <li class="mb-6"><a class="text-gray-200 hover:text-gray-100" href="#">New account</a></li>
-                    <li class="mb-6"><a class="text-gray-200 hover:text-gray-100" href="#">Log in</a></li>
-                    <li class="mb-6"><a class="text-gray-200 hover:text-gray-100" href="#">Testimonials</a></li>
-                    <li class="mb-6"><a class="text-gray-200 hover:text-gray-100" href="#">Contact</a></li>
-                    <li class="mb-6"><a class="text-gray-200 hover:text-gray-100" href="#">Privacy Policy</a></li>
+                    <li class="mb-2"><a class="text-gray-200 hover:text-gray-100" href="#">New account</a></li>
+                    <li class="mb-2"><a class="text-gray-200 hover:text-gray-100" href="#">Log in</a></li>
+                    <li class="mb-2"><a class="text-gray-200 hover:text-gray-100" href="#">Testimonials</a></li>
+                    <li class="mb-2"><a class="text-gray-200 hover:text-gray-100" href="#">Contact</a></li>
+                    <li class="mb-2"><a class="text-gray-200 hover:text-gray-100" href="#">Privacy Policy</a></li>
                     <li><a class="text-gray-200 hover:text-gray-100" href="#">Cookies</a></li>
                   </ul>
                 </div>
@@ -44,9 +50,9 @@
             </div>
           </div>
           <div class="md:flex justify-between">
-            <p class="text-lg text-gray-200 mb-8 md:mb-0">&copy; 2021 Shuffle. All rights reserved.</p>
+            <p class="text-lg text-gray-200 md:mb-0">&copy; 2021 Shuffle. All rights reserved.</p>
             <div class="flex items-center">
-              <a class="inline-block mr-2" href="#">
+              <!-- <a class="inline-block text-white mr-2" href="#">
                 <img class="h-12" src="zospace-assets/logos/facebook.svg" alt="">
               </a>
               <a class="inline-block mr-2" href="#">
@@ -54,7 +60,7 @@
               </a>
               <a class="inline-block" href="#">
                 <img class="h-12" src="zospace-assets/logos/twitter.svg" alt="">
-              </a>
+              </a> -->
             </div>
           </div>
         </div>
