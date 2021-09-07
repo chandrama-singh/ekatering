30f0899dd3f807aba397a2740bd694e2088861c5
Abhimanyu Kushwaha <88226869+abhimanyu-kushwaha@users.noreply.github.com>
2021-09-07 12:53:09 +0530

    Fixed Home page

---
+++ components/Subscribe.vue
@@ -0,0 +1,29 @@
+<template>
+   <section class="py-12 px-4">
+        <div class="w-full max-w-2xl mx-auto text-center">
+          <!-- <span class="text-sm font-semibold">TAGLINE</span> -->
+          <h2 class="text-5xl mt-4 mb-8 leading-tight font-semibold font-heading">No paper plane can be made without paper</h2>
+          <form class="w-full max-w-xl mx-auto">
+            <div class="flex flex-wrap mb-4">
+              <div class="w-full md:w-2/3">
+                <input class="appearance-none block w-full py-3 px-4 mb-4 md:mb-0 leading-snug text-gray-700 bg-gray-50 focus:bg-white border border-gray-200 focus:border-gray-500 rounded md:rounded-r-none focus:outline-none" type="text" placeholder="michael@dundermifflin.com">
+              </div>
+              <div class="w-full md:w-1/3">
+                <button class="inline-block w-full py-4 px-8 leading-none text-white bg-indigo-600 hover:bg-indigo-700 font-semibold rounded md:rounded-l-none">Subscribe</button>
+              </div>
+            </div>
+            <p class="text-sm text-gray-400 leading-relaxed">Dunder Mifflin Paper Productions is the way to provide your company an extraordinary paper, sold by extraordinary people.</p>
+          </form>
+        </div>
+      </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/Work.vue
@@ -0,0 +1,65 @@
+<template>
+<div>
+<h2 class="text-center text-blue-500 mt-10 mb-10 text-5xl lg:text-5xl font-bold font-heading">How it Works</h2>
+  <section class="relative pt-20 pb-5 2xl:pb-40 overflow-hidden">
+        <div class="container px-4 mx-auto">
+          <div class="flex flex-wrap mb-16 lg:mb-28">
+            <div class="w-full lg:w-1/2 mb-16 lg:mb-0">
+              <div class="max-w-md">
+                <!-- <span class="text-lg font-bold text-blue-500">How it works</span> -->
+                <h2 class=" mb-10 text-3xl lg:text-4xl font-bold font-heading">Build and launch without problems</h2>
+                <p class="mb-10 text-xl text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing. Dolor sit amet consectetur adipisicing elit. Aliquid harum ducimus adipisci animi quidem nesciunt similique ex enim accusantium voluptate. Consectetur adipisicing harum ducimus adipisci.</p>
+                <a class="inline-block px-12 py-5 text-white font-bold bg-blue-500 hover:bg-blue-600 rounded-full transition duration-200" href="#">How it works?</a>
+              </div>
+            </div>
+            <img class="lg:absolute top-0 right-5 lg:w-1/2 w-full h-96 lg:h-144 object-cover" src="https://images.pexels.com/photos/7148031/pexels-photo-7148031.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="">
+          </div>
+          <!-- <div class="flex flex-wrap -m-4">
+            <div class="w-full lg:w-1/3 p-4">
+              <div class="flex pt-8 lg:pt-20 border-t border-gray-50">
+                <div class="mr-8">
+                  <span class="flex justify-center items-center w-14 h-14 bg-blue-50 text-lg font-bold rounded-full">1</span>
+                </div>
+                <div class="max-w-xs">
+                  <h3 class="mb-6 text-lg font-bold font-heading">Register account</h3>
+                  <p class="text-lg">It&rsquo;s over, maecenas tincidunt malesuada dolor sit amet malesuada.</p>
+                </div>
+              </div>
+            </div>
+            <div class="w-full lg:w-1/3 p-4">
+              <div class="flex pt-8 lg:pt-20 border-t border-gray-50">
+                <div class="mr-8">
+                  <span class="flex justify-center items-center w-14 h-14 bg-blue-50 text-lg font-bold rounded-full">2</span>
+                </div>
+                <div class="max-w-xs">
+                  <h3 class="mb-6 text-lg font-bold font-heading">Customize tools</h3>
+                  <p class="text-lg">The brown me quam, sagittis porttitor lorem vel, commodo fringilla nisl.</p>
+                </div>
+              </div>
+            </div>
+            <div class="w-full lg:w-1/3 p-4">
+              <div class="flex pt-8 lg:pt-20 border-t border-gray-50">
+                <div class="mr-8">
+                  <span class="flex justify-center items-center w-14 h-14 bg-blue-50 text-lg font-bold rounded-full">3</span>
+                </div>
+                <div class="max-w-xs">
+                  <h3 class="mb-6 text-lg font-bold font-heading">Work with your team</h3>
+                  <p class="text-lg">The time, this accumsan augue, posuere posuere elit lorem.</p>
+                </div>
+              </div>
+            </div>
+          </div> -->
+        </div>
+      </section>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
