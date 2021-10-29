9f3c3780427154895f65e49d14411b093bf984bb
Abhimanyu Kushwaha <88226869+abhimanyu-kushwaha@users.noreply.github.com>
2021-10-29 13:06:12 +0530

    Product Added

--- pages/caterers/products.vue
+++ pages/caterers/products.vue
@@ -1,223 +1,61 @@
 <template>
   <div>
-    <!-- Product Head Section -->
-    <div class=" min-w-screen bg-gray-200 px-5 pb-5 pt-2">
-      <div class="py-3 px-5 bg-white rounded shadow-xl">
-        <div class="-mx-1">
-          <ul class="flex w-full flex-wrap justify-between h-10">
-            <li class="block relative">
-              <h2 href="#" class="flex items-center font-bold">
-                Manage Products
-              </h2>
-            </li>
-<div class="justify-between">
-            <li class="block relative">
-              <a
-                href="#"
-                class="
-                  flex
-                  items-center
-                  h-10
-                  leading-10
-                  px-4
-                  rounded
-                  cursor-pointer
-                  no-underline
-                  hover:no-underline
-                  transition-colors
-                  duration-100
-                  mx-1
-                  bg-purple-400
-                  hover:bg-purple-800
-                "
-              >
-                Add new product<i class="fas fa-plus ml-3"></i>
-              </a>
-            </li>
-            </div>
+    <div class="w-full flex justify-center max-h-screen">
+        <div class="w-full">
+            <div class="w-full flex flex-col md:flex-row  overflow-hidden shadow-xl">
+                
+                <div class="w-full md:w-1/5 h-auto">
+                    <div class="top flex items-center px-5 h-16 bg-purple-800 text-white">
+                        <div class="ml-3 flex flex-col text-xl">
+                           Product
+                        </div>
+                    </div>
+                    <div class="bg-gray-300 w-full h-full sm:flex md:block">
+                        <button id="button-1" v-on:click="toggleTabs(1)" v-bind:class="{' bg-purple-200': openTab !== 1, 'text-white bg-pink-600': openTab === 1}"  class="w-full border-b-2 flex justify-between items-center px-5 py-2 hover:bg-purple-500 hover:text-white cursor-pointer focus:outline-none">
+                            <span><i class="fa fa-inbox w-6"></i>Products</span>
+                        </button>
+                        <button id="button-2" v-on:click="toggleTabs(2)" v-bind:class="{' bg-purple-200': openTab !== 2, 'text-white bg-pink-600': openTab === 2}"  class="w-full border-b-2 flex justify-between items-center px-5 py-2 hover:bg-purple-500 hover:text-white cursor-pointer focus:outline-none">
+                            <span><i class="fa fa-envelope w-6"></i>Categories</span>
+                            
+                        </button>
+                        <button id="button-3" v-on:click="toggleTabs(3)" v-bind:class="{' bg-purple-200': openTab !== 3, 'text-white bg-pink-600': openTab === 3}"  class="w-full border-b-2 flex justify-between items-center px-5 py-2 hover:bg-purple-500 hover:text-white cursor-pointer focus:outline-none">
+                            <span><i class="fa fa-bookmark w-6"></i>Discounts</span>
+                        </button>
+                        <!-- <button id="button-4" onclick="showView(4)" class="w-full flex justify-between items-center px-5 py-2 hover:bg-purple-500 hover:text-white cursor-pointer focus:outline-none">
+                            <span><i class="fa fa-trash w-6"></i>Orders</span>
+                            
+                        </button> -->
+                    </div>
+                </div>
+                
 
-            <div class="justify-between">
-              <input type="text" name="search" placeholder="Search..." />
-              <span class="">
-                <button class="bg-purple-600 text-white px-2" type="submit">
-                  Search
-                </button>
-              </span>
+                <div class="w-full md:w-4/5">
+                    <div class="top flex items-center px-5 h-16 bg-purple-600 text-white text-2xl">
+                        <!--------------------------Products--------------------------------->
+                        <div class="px-2" v-bind:class="{'hidden': openTab !== 1, 'block': openTab === 1}">
+                          Products
+                        </div>
+                        <!--------------------------Categories--------------------------------->
+                        <div class="px-2" v-bind:class="{'hidden': openTab !== 1, 'block': openTab === 1}">
+                          Products
+                        </div>
+                        <!--------------------------Discount--------------------------------->
+                        <div class="px-2" v-bind:class="{'hidden': openTab !== 1, 'block': openTab === 1}">
+                          Products
+                        </div>
+                    </div>
+                    <div class="w-full px-5 py-3 max-h-screen overflow-y-auto">
+                      <!--------------------------Products--------------------------------->
+                        <div class="px-2" v-bind:class="{'hidden': openTab !== 1, 'block': openTab === 1}">
+                          Products
+                        </div>
+                        <!--------------------------Categories--------------------------------->
+                        <!--------------------------Discount--------------------------------->
+                    </div>
+                </div>
+
             </div>
-          </ul>
         </div>
-      </div>
-    </div>
-
-    <div class="h-screen w-full flex">
-      <!-- main -->
-      <main class="w-full overflow-y-auto">
-        <div class="px-10 grid grid-cols-4 gap-4">
-          <div
-            class="
-              col-span-4
-              sm:col-span-4
-              md:col-span-2
-              lg:col-span-1
-              xl:col-span-1
-              flex flex-col
-              items-center
-            "
-          >
-            <div class="bg-white rounded-lg mt-5">
-              <img
-                src="https://source.unsplash.com/MNtag_eXMKw/1600x900"
-                class="h-40 rounded-md"
-                alt=""
-              />
-            </div>
-            <div class="bg-white shadow-lg rounded-lg -mt-4 w-64">
-              <div class="py-5 px-5">
-                <span class="font-bold text-gray-800 text-lg">Geek Pizza</span>
-                <div class="flex items-center justify-between">
-                  <div class="text-sm text-gray-600 font-light">
-                    Size : Regular
-                  </div>
-                  <div class="text-2xl text-red-600 font-bold">€ 8.00</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- end cols -->
-
-          <div
-            class="
-              col-span-4
-              sm:col-span-4
-              md:col-span-2
-              lg:col-span-1
-              xl:col-span-1
-              flex flex-col
-              items-center
-            "
-          >
-            <div class="bg-white rounded-lg mt-5">
-              <img
-                src="https://source.unsplash.com/FOOD"
-                class="h-40 rounded-md"
-                alt=""
-              />
-            </div>
-            <div class="bg-white shadow-lg rounded-lg -mt-4 w-64">
-              <div class="py-5 px-5">
-                <span class="font-bold text-gray-800 text-lg">Burger</span>
-                <div class="flex items-center justify-between">
-                  <div class="text-sm text-gray-600 font-light">
-                    Size : Regular
-                  </div>
-                  <div class="text-2xl text-red-600 font-bold">€ 10.00</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- end cols -->
-
-          <div
-            class="
-              col-span-4
-              sm:col-span-4
-              md:col-span-2
-              lg:col-span-1
-              xl:col-span-1
-              flex flex-col
-              items-center
-            "
-          >
-            <div class="bg-white rounded-lg mt-5">
-              <img
-                src="https://source.unsplash.com/FOOD"
-                class="h-40 rounded-md"
-                alt=""
-              />
-            </div>
-            <div class="bg-white shadow-lg rounded-lg -mt-4 w-64">
-              <div class="py-5 px-5">
-                <span class="font-bold text-gray-800 text-lg">Chinese</span>
-                <div class="flex items-center justify-between">
-                  <div class="text-sm text-gray-600 font-light">
-                    Size : Regular
-                  </div>
-                  <div class="text-2xl text-red-600 font-bold">€ 8.00</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- end cols -->
-
-          <div
-            class="
-              col-span-4
-              sm:col-span-4
-              md:col-span-2
-              lg:col-span-1
-              xl:col-span-1
-              flex flex-col
-              items-center
-            "
-          >
-            <div class="bg-white rounded-lg mt-5">
-              <img
-                src="https://source.unsplash.com/FOOD"
-                class="h-40 rounded-md"
-                alt=""
-              />
-            </div>
-            <div class="bg-white shadow-lg rounded-lg -mt-4 w-64">
-              <div class="py-5 px-5">
-                <span class="font-bold text-gray-800 text-lg">Weg</span>
-                <div class="flex items-center justify-between">
-                  <div class="text-sm text-gray-600 font-light">
-                    Size : Regular
-                  </div>
-                  <div class="text-2xl text-red-600 font-bold">€ 8.00</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- end cols -->
-
-          <div
-            class="
-              col-span-4
-              sm:col-span-4
-              md:col-span-2
-              lg:col-span-1
-              xl:col-span-1
-              flex flex-col
-              items-center
-            "
-          >
-            <div class="bg-white rounded-lg mt-5">
-              <img
-                src="https://source.unsplash.com/FOOD"
-                class="h-40 rounded-md"
-                alt=""
-              />
-            </div>
-            <div class="bg-white shadow-lg rounded-lg -mt-4 w-64">
-              <div class="py-5 px-5">
-                <span class="font-bold text-gray-800 text-lg">Egg with Bread</span>
-                <div class="flex items-center justify-between">
-                  <div class="text-sm text-gray-600 font-light">
-                    Size : Regular
-                  </div>
-                  <div class="text-2xl text-red-600 font-bold">€ 13.00</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- end cols -->
-        </div>
-      </main>
-    </div>
-    <div class="flex justify-center gap-4 mb-2">
-      <button class="bg-purple-600 py-1 px-4 rounded-sm">Prev</button>
-      <button class="bg-purple-600 py-1 px-4 rounded-sm">Next</button>
     </div>
   </div>
 </template>
