3e218ee47df11c3d37cffa42c44b11c641d960e9
Abhimanyu Kushwaha <88226869+abhimanyu-kushwaha@users.noreply.github.com>
2021-11-03 21:51:20 +0530

    some update

---
+++ pages/food-sms-form/iet-2.vue
@@ -0,0 +1,346 @@
+<template>
+  <section class="py-10 bg-white">
+      <div class="min-h-screen flex items-center justify-center px-4">
+    
+    <div class="max-w-4xl  bg-blue-50 w-full rounded-lg shadow-xl border-2 border-purple-400">
+        <div class="p-4 border-b flex justify-between bg-purple-500 text-white">
+            <h2 class="text-2xl font-semibold">
+                Office Use 
+            </h2>
+            <p class="text-sm text-white">
+                Ref: #025/2020
+            </p>
+        </div>
+        <div>
+            <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-3 border-b">
+                <p class="text-gray-600 ">
+                    Ref:(Office Use)
+                </p>
+                 <input
+                      placeholder="Reference Number"
+                      class="
+                        appearance-none
+                        block
+                        w-full
+                        bg-grey-lighter
+                        text-grey-darker
+                        border border-grey-lighter
+                        rounded-lg
+                        h-10
+                        px-4
+                      "
+                      required="required"
+                      type="text"
+                      name="integration[shop_name]"
+                      id="integration_shop_name"
+                    />
+            </div>
+             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-3 border-b">
+                <p class="text-gray-600 ">
+                   Date
+                </p>
+                 <input
+                      placeholder="DD/MM/YYYY"
+                      class="
+                        appearance-none
+                        block
+                        w-full
+                        bg-grey-lighter
+                        text-grey-darker
+                        border border-grey-lighter
+                        rounded-lg
+                        h-10
+                        px-4
+                      "
+                      required="required"
+                      type="text"
+                      name="integration[shop_name]"
+                      id="integration_shop_name"
+                    />
+            </div>
+             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-3 border-b">
+                <p class="text-gray-600 ">
+                   Name (Required)
+                </p>
+                 <input
+                      placeholder="Enter Name"
+                      class="
+                        appearance-none
+                        block
+                        w-full
+                        bg-grey-lighter
+                        text-grey-darker
+                        border border-grey-lighter
+                        rounded-lg
+                        h-10
+                        px-4
+                      "
+                      required="required"
+                      type="text"
+                      name="integration[shop_name]"
+                      id="integration_shop_name"
+                    />
+            </div>
+             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-3 border-b">
+                <p class="text-gray-600 ">
+                    Job Titile
+                </p>
+                 <input
+                      placeholder="Enter Job Title"
+                      class="
+                        appearance-none
+                        block
+                        w-full
+                        bg-grey-lighter
+                        text-grey-darker
+                        border border-grey-lighter
+                        rounded-lg
+                        h-10
+                        px-4
+                      "
+                      required="required"
+                      type="email"
+                      name="integration[shop_name]"
+                      id="integration_shop_name"
+                    />
+            </div>
+            <!--------------------------------------------------------------------------------------->
+            <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-3 border-b">
+                <p class="text-gray-600 ">
+                    Phone number (required)
+                </p>
+                 <input
+                      placeholder="Phone Number"
+                      class="
+                        appearance-none
+                        block
+                        w-full
+                        bg-grey-lighter
+                        text-grey-darker
+                        border border-grey-lighter
+                        rounded-lg
+                        h-10
+                        px-4
+                      "
+                      required="required"
+                      type="text"
+                      name="integration[shop_name]"
+                      id="integration_shop_name"
+                    />
+            </div>
+             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-3 border-b">
+                <p class="text-gray-600 ">
+                   Email Address (required)
+                </p>
+                 <input
+                      placeholder="Email Address"
+                      class="
+                        appearance-none
+                        block
+                        w-full
+                        bg-grey-lighter
+                        text-grey-darker
+                        border border-grey-lighter
+                        rounded-lg
+                        h-10
+                        px-4
+                      "
+                      required="required"
+                      type="text"
+                      name="integration[shop_name]"
+                      id="integration_shop_name"
+                    />
+            </div>
+             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-3 border-b">
+                <p class="text-gray-600 ">
+                   Company Name
+                </p>
+                 <input
+                      placeholder="Company Name"
+                      class="
+                        appearance-none
+                        block
+                        w-full
+                        bg-grey-lighter
+                        text-grey-darker
+                        border border-grey-lighter
+                        rounded-lg
+                        h-10
+                        px-4
+                      "
+                      required="required"
+                      type="text"
+                      name="integration[shop_name]"
+                      id="integration_shop_name"
+                    />
+            </div>
+             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-3 border-b">
+                <p class="text-gray-600 ">
+                    Trading Name (if different from above)
+                </p>
+                 <input
+                      placeholder="Trading Name"
+                      class="
+                        appearance-none
+                        block
+                        w-full
+                        bg-grey-lighter
+                        text-grey-darker
+                        border border-grey-lighter
+                        rounded-lg
+                        h-10
+                        px-4
+                      "
+                      required="required"
+                      type="email"
+                      name="integration[shop_name]"
+                      id="integration_shop_name"
+                    />
+            </div>
+             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-3 border-b">
+                <p class="text-gray-600 ">
+                   Company Registration Number
+                </p>
+                 <input
+                      placeholder="Company Registration Number"
+                      class="
+                        appearance-none
+                        block
+                        w-full
+                        bg-grey-lighter
+                        text-grey-darker
+                        border border-grey-lighter
+                        rounded-lg
+                        h-10
+                        px-4
+                      "
+                      required="required"
+                      type="text"
+                      name="integration[shop_name]"
+                      id="integration_shop_name"
+                    />
+            </div>
+             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-3 border-b">
+                <p class="text-gray-600 ">
+                    Business Trading Address
+                </p>
+                 <input
+                      placeholder="Business Trading Address"
+                      class="
+                        appearance-none
+                        block
+                        w-full
+                        bg-grey-lighter
+                        text-grey-darker
+                        border border-grey-lighter
+                        rounded-lg
+                        h-10
+                        px-4
+                      "
+                      required="required"
+                      type="email"
+                      name="integration[shop_name]"
+                      id="integration_shop_name"
+                    />
+            </div>
+            <!--------------------------------------------------------------------------------------->
+            <div class="p-3 border-b flex justify-between bg-purple-400 text-white">
+            <h2 class="text-lg font-bold">
+                Scope of Business (in 50 words or less): Who do you supply to; amount; locality
+            </h2>
+        </div>
+         <div class="md:grid md:grid-cols-1 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+              <textarea
+                      placeholder="Scope of Business"
+                      class="
+                        appearance-none
+                        block
+                        w-full
+                        bg-grey-lighter
+                        text-grey-darker
+                        border border-grey-lighter
+                        rounded-lg
+                        h-24
+                        px-4
+                      "
+                      required="required"
+                      type="text"
+                     rows="10"
+                    ></textarea>
+            </div>
+            <div class="p-3 border-b flex justify-between bg-purple-400 text-white">
+            <h2 class="text-lg ">
+                <b>Product(s) description and intended use:</b> Please describe each product range in 150 words or less. We consider a “range of products” to be similar in flavour and similar process.
+            </h2>
+        </div>
+         <div class="md:grid md:grid-cols-1 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+              <textarea
+                      placeholder="Description"
+                      class="
+                        appearance-none
+                        block
+                        w-full
+                        bg-grey-lighter
+                        text-grey-darker
+                        border border-grey-lighter
+                        rounded-lg
+                        h-24
+                        px-4
+                      "
+                      required="required"
+                      type="text"
+                     rows="8"
+                    ></textarea>
+            </div>
+        <!--------------------------------------->
+        <div class="p-3 border-b flex justify-between bg-purple-400 text-white">
+            <h2 class="text-lg ">
+                <b>Is the product manufactured in accordance with an ISO standard?</b> Yes/No <br/><b>If no, how is quality managed? E.g.,</b> Quality is managed with an in-house quality program.
+            </h2>
+        </div>
+         <div class="md:grid md:grid-cols-1 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+              <textarea
+                      placeholder="Description"
+                      class="
+                        appearance-none
+                        block
+                        w-full
+                        bg-grey-lighter
+                        text-grey-darker
+                        border border-grey-lighter
+                        rounded-lg
+                        h-24
+                        px-4
+                      "
+                      required="required"
+                      type="text"
+                     rows="8"
+                    ></textarea>
+            </div>
+             <div class="p-4 border-b flex justify-center py-6">
+            <button type="submit"
+                            class="mr-4 inline-flex justify-center w-24 py-2 px-4 border border-transparent shadow-sm text-sm font-semibold rounded-md   text-white bg-gray-600 hover:bg-gray-700 ">
+                            Cancel
+                        </button>
+                        <button type="submit"
+                            class="inline-flex justify-center w-24 py-2 px-4 border border-transparent shadow-sm text-sm font-semibold rounded-md  bg-indigo-600 hover:bg-indigo-700 text-white">
+                            Submit
+                        </button>
+        </div>
+        </div>
+    </div>
+
+</div>
+  </section>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/legal-advice-form/la-01.vue
@@ -0,0 +1,152 @@
+<template>
+  <section class="py-10 bg-white">
+      <div class="min-h-screen flex items-center justify-center px-4">
+    
+    <div class="max-w-4xl  bg-blue-50 w-full rounded-lg shadow-xl border-2 border-purple-400 ">
+        <div class="p-4 border-b flex justify-between bg-purple-500 text-white">
+            <h2 class="text-2xl font-semibold">
+                Contact Us
+                <p class="text-sm"></p>
+            </h2>
+            <!-- <p class="text-sm text-white">
+                Ref: #025/2020
+            </p> -->
+        </div>
+        <div>
+            <!-- <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                <p class="text-gray-600 ">
+                    Select the amount of product assessments required: 
+                </p>
+                 <input
+                      placeholder=""
+                      class="
+                        appearance-none
+                        block
+                        w-full
+                        bg-grey-lighter
+                        text-grey-darker
+                        border border-grey-lighter
+                        rounded-lg
+                        h-10
+                        px-4
+                      "
+                      required="required"
+                      type="text"
+                      name="integration[shop_name]"
+                      id="integration_shop_name"
+                    />
+            </div> -->
+             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                <p class="text-gray-600 ">
+                   Name<span class="text-red-500">*</span>
+                </p>
+                 <input
+                      placeholder="Enter Name"
+                      class="
+                        appearance-none
+                        block
+                        w-full
+                        bg-grey-lighter
+                        text-grey-darker
+                        border border-grey-lighter
+                        rounded-lg
+                        h-10
+                        px-4
+                      "
+                      required="required"
+                      type="text"
+                      name="integration[shop_name]"
+                      id="integration_shop_name"
+                    />
+            </div>
+             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                <p class="text-gray-600 ">
+                    Email<span class="text-red-500">*</span>
+                </p>
+                 <input
+                      placeholder="Email"
+                      class="
+                        appearance-none
+                        block
+                        w-full
+                        bg-grey-lighter
+                        text-grey-darker
+                        border border-grey-lighter
+                        rounded-lg
+                        h-10
+                        px-4
+                      "
+                      required="required"
+                      type="email"
+                      name="integration[shop_name]"
+                      id="integration_shop_name"
+                    />
+            </div>
+             <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+                <p class="text-gray-600 ">
+                    I Need Help With :
+                </p>
+                 <select
+                  name=""
+                  id=""
+                  class="form-select appearance-none block w-full px-5 py-2 border rounded-lg bg-white shadow-lg  placeholder-blueGray-300
+                          text-blueGray-600 focus:ring focus:outline-none"
+                  x-model="expired.month"
+                >
+                  <option value="" selected disabled>A food safety report</option>
+                  <option value="01">Improvement notice</option>
+                  <option value="02">Prohibition notice</option>
+                  <option value="03">Closure of my business</option>
+                  <option value="04">Other</option>
+                 
+                </select>
+            </div>
+            
+         <div class="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+              <p class="text-gray-600 ">
+                    Message
+                </p>
+              <textarea
+                      placeholder="Message"
+                      class="
+                        appearance-none
+                        block
+                        w-full
+                        bg-grey-lighter
+                        text-grey-darker
+                        border border-grey-lighter
+                        rounded-lg
+                        h-20
+                        px-4
+                      "
+                      required="required"
+                      type="text"
+                     rows="8"
+                    ></textarea>
+            </div>
+            
+        <!--------------------------------------->
+    
+             <div class="p-4 border-b flex justify-center py-6">
+                        <button type="submit"
+                            class="inline-flex justify-center w-24 py-2 px-4 border border-transparent shadow-sm text-sm font-semibold rounded-md  bg-indigo-600 hover:bg-indigo-700 text-white">
+                            Submit
+                        </button>
+        </div>
+        </div>
+    </div>
+
+</div>
+  </section>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
