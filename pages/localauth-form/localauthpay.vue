54263a5d8f03c930f0a2b6906d35101a13973007
Abhimanyu Kushwaha <88226869+abhimanyu-kushwaha@users.noreply.github.com>
2021-11-03 21:10:42 +0530

    update payment section

--- pages/localauth-form/localauthpay.vue
+++ pages/localauth-form/localauthpay.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="py-1 bg-blueGray-50">
+  <section class="py-1 bg-white">
     <div class="w-full lg:w-8/12 px-4 mx-auto mt-6">
       <div
         class="
@@ -9,18 +9,29 @@
           break-words
           w-full
           mb-6
-          shadow-lg
-          rounded-lg
-          bg-blueGray-100
-          border-0
+          
+          bg-blue-50
+          rounded-lg shadow-xl border-2 border-purple-400
         "
       >
-        <div class="rounded-t bg-white mb-0 px-6 py-6">
+        <div class="rounded-t bg-purple-500 mb-0 px-6 py-6">
           <div class="text-center flex justify-center">
-            <h6 class="text-blueGray-700 text-xl font-bold">Checkout</h6>
+            <h6 class="text-white text-xl font-bold">Checkout</h6>
           </div>
         </div>
-
+        <div class="text-center flex justify-center mt-3">
+           <ul class="flex">
+            <li class="mx-2">
+              <img class="w-24" src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/computop.png" alt="" />
+            </li>
+            <li class="mx-2">
+              <img class="w-24" src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/verified-by-visa.png" alt="" />
+            </li>
+            <li class="ml-5">
+              <img class="w-14" src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/mastercard-id-check.png" alt="" />
+            </li>
+          </ul>
+        </div>
         <div
           class="
             flex flex-wrap
@@ -95,32 +106,32 @@
           <form>
             <div class="flex flex-wrap">
               <div class="w-full lg:w-12/12 px-4">
-                <div class="relative w-full mb-3 text-center">
-                  <label
+                <div class="relative w-full mb-4 text-center">
+                  <!-- <label
                     class="
                       block
                       uppercase
                       text-blueGray-600 text-sm
                       font-bold
                       mb-2
+                      text-left
                     "
                     htmlfor="grid-password"
                   >
                     Cardholder Name
-                  </label>
+                  </label> -->
                   <input
                     type="text"
                     placeholder="Name on the Card"
                     class="
-                      border-0
+                      border rounded-lg bg-white shadow-lg
                       px-3
                       py-3
                       placeholder-blueGray-300
                       text-blueGray-600
-                      bg-white
-                      rounded
+                      
                       text-sm
-                      shadow
+                      
                       focus:outline-none focus:ring
                       w-full
                       ease-linear
@@ -131,32 +142,32 @@
                   />
                 </div>
 
-                <div class="relative w-full mb-3 text-center">
-                  <label
+                <div class="relative w-full mb-4 text-center">
+                  <!-- <label
                     class="
                       block
                       uppercase
                       text-blueGray-600 text-sm
                       font-bold
                       mb-2
+                      text-left
                     "
                     htmlfor="grid-password"
                   >
                     Card Details
-                  </label>
+                  </label> -->
                   <input
                     type="text"
                     placeholder="Card Number"
                     class="
-                      border-0
+                      border rounded-lg bg-white shadow-lg
                       px-3
                       py-3
                       placeholder-blueGray-300
                       text-blueGray-600
-                      bg-white
-                      rounded
+                      
                       text-sm
-                      shadow
+                     
                       focus:outline-none focus:ring
                       w-full
                       ease-linear
@@ -167,46 +178,50 @@
                   />
                 </div>
 
-                <div class="flex flex-wrap">
+                <div class="flex flex-wrap mb-3">
                   <div class="w-full lg:w-6/12">
-                    <div class="relative w-full mb-3">
-                      <label
-                        class="
-                          block
-                          uppercase
-                          text-blueGray-600 text-xs
-                          font-bold
-                          mb-2
-                        "
-                        htmlfor="grid-password"
-                      >
-                        MM / YY
-                      </label>
-                      <input
-                        type="text"
-                        placeholder="MM / YY"
-                        class="
-                          border-0
-                          px-3
-                          py-3
-                          placeholder-blueGray-300
-                          text-blueGray-600
-                          bg-white
-                          rounded
-                          text-sm
-                          shadow
-                          focus:outline-none focus:ring
-                          w-full
-                          ease-linear
-                          transition-all
-                          duration-150
-                        "
-                      />
+                    <div class="relative w-full mb-3 flex gap-2">
+                      <select
+                  name=""
+                  id=""
+                  class="form-select appearance-none block w-1/2 px-5 py-2 border rounded-lg bg-white shadow-lg  placeholder-blueGray-300
+                          text-blueGray-600 focus:ring focus:outline-none"
+                  x-model="expired.month"
+                >
+                  <option value="" selected disabled>MM</option>
+                  <option value="01">01</option>
+                  <option value="02">02</option>
+                  <option value="03">03</option>
+                  <option value="04">04</option>
+                  <option value="05">05</option>
+                  <option value="06">06</option>
+                  <option value="07">07</option>
+                  <option value="08">08</option>
+                  <option value="09">09</option>
+                  <option value="10">10</option>
+                  <option value="11">11</option>
+                  <option value="12">12</option>
+                </select>
+                <select
+                  name=""
+                  id=""
+                  class="form-select appearance-none block w-1/2 px-5 py-2 border rounded-lg bg-white shadow-lg  placeholder-blueGray-300
+                          text-blueGray-600 focus:ring focus:outline-none"
+                  x-model="expired.year"
+                >
+                  <option value="" selected disabled>YY</option>
+                  <option value="2021">2021</option>
+                  <option value="2022">2022</option>
+                  <option value="2023">2023</option>
+                  <option value="2024">2024</option>
+                  <option value="2025">2025</option>
+                  <option value="2026">2026</option>
+                </select>
                     </div>
                   </div>
                   <div class="w-full lg:w-6/12 px-4">
                     <div class="relative w-full mb-3">
-                      <label
+                      <!-- <label
                         class="
                           block
                           uppercase
@@ -217,19 +232,19 @@
                         htmlfor="grid-password"
                       >
                         CVV
-                      </label>
+                      </label> -->
                       <input
                         type="cvv"
+                        placeholder="CVV"
                         class="
-                          border-0
+                          border rounded-lg bg-white shadow-lg
                           px-3
                           py-3
                           placeholder-blueGray-300
                           text-blueGray-600
-                          bg-white
-                          rounded
+                          
                           text-sm
-                          shadow
+                         
                           focus:outline-none focus:ring
                           w-full
                           ease-linear
