3a983b1393581857c315b504a00052892dc1ebdc
Durga Prasad Mishra <dpmishraosizone@gmail.com>
2021-12-27 17:32:40 +0530

    register payment success page fix

--- pages/caterers/register/payment-success/_id/index.vue
+++ pages/caterers/register/payment-success/_id/index.vue
@@ -6,7 +6,7 @@
                 d="M12,0A12,12,0,1,0,24,12,12.014,12.014,0,0,0,12,0Zm6.927,8.2-6.845,9.289a1.011,1.011,0,0,1-1.43.188L5.764,13.769a1,1,0,1,1,1.25-1.562l4.076,3.261,6.227-8.451A1,1,0,1,1,18.927,8.2Z">
             </path>
         </svg>
-        <div v-if="response">
+        <div>
             <h3 class="md:text-2xl text-base text-gray-900 font-semibold text-center">Payment Done!</h3>
             <!-- <h3 class="md:text-2xl text-base text-gray-900 font-semibold text-center">{{data}}</h3> -->
             <p class="text-gray-600 text-center">Thank you for completing your secure online payment. Have a great day!  </p>
@@ -14,6 +14,7 @@
               <p class="my-3 text-lg ">Take screenshot for future reference</p>
 
               <span class=" font-bold flex p-3">catererId: <p class="mx-3"> {{response.catererId}} </p> </span>
+              
               <span class=" font-bold flex p-3">status: <p class="mx-3">  {{response.status}} </p> </span>
               <span class=" font-bold flex p-3">customer: <p class="mx-3">  {{response.customer}} </p> </span>
               <span class=" font-bold flex p-3">payment_intent: <p class="mx-3">  {{response.payment_intent}} </p> </span>
